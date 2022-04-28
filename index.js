ef1956b86880f40b02f9ee60968193fa4dc32c19
Iris-Gonzalez-Sanchez <Iris.GonzalezSan@Gmail.com>
2022-04-28 17:50:47 -0400

    commit tree 6daac44169ecd29f205774e893bef04dcbeccc39

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+let gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    };
+
+    return gifts;
+};
+
+wrapGifts(gifts);
+
+let arrayOfNames = ["Matilda", "Veronica", "Brittney"];
+let party = "birthday";
+
+function writeCards(arrayOfNames, party) {
+    let newArray = []
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        newArray.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${party} gift!`);
+        console.log(newArray)
+    };
+    return newArray;
+};
+writeCards(arrayOfNames, party);
+
+let number = 10;
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
+console.log(countDown(number));
